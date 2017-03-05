3132e1dd3d9980b8af479f2756c996fd2fe2b7d4
zerattul <zerattul_8@hotmail.es>
2017-03-04 18:13:41 -0600

    solo evento

--- models/model.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const mongoose = require('mongoose'),
-	conf         = require('./db-conf');
-
-mongoose.connect(`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`);
-
-module.exports = mongoose;
--- server.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const app = require('./app'),
-	server = app.listen( app.get('port'), () => console.log(`Iniciando API REST-MVC Receptium y Autenticación en el puerto ${app.get('port')}`) );
